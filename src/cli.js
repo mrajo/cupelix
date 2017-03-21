1e6f9e0e8b282699820cd230f8ac53e03d8c91df
mrajo <anthony.c@stle.co>
2017-03-21 16:09:53 -0700

    add tests for authentication

--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,15 @@ import { argv as yargs } from 'yargs'
 const ARGV_DEFAULTS = {
   env: 'prod',
   port: 5555,
-  auth: 'data/auth.json'
+  auth: 'data/prod_auth.json',
+  data: 'data/prod_index.json'
 }
 
-export const argv = Object.assign(ARGV_DEFAULTS, yargs)
+let argv_user = {}
+
+if (yargs.env) {
+  argv_user.auth = `data/${yargs.env}_auth.json`
+  argv_user.data = `data/${yargs.env}_index.json`
+}
+
+export const argv = Object.assign(ARGV_DEFAULTS, argv_user, yargs)
